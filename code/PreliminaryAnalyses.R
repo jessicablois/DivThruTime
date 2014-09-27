75af1618786f0143d2c9d3b77bc9731aa749e6f8
Jessica Blois <jblois@ucmerced.edu>
2014-09-27 09:22:57 -0700

    Switched local files so that synced with Git

--- code/PreliminaryAnalyses.R
+++ code/PreliminaryAnalyses.R
@@ -3,5 +3,9 @@
 # pollenDir<- "/Volumes/bloisgroup/bloislab/Data/Biological" #fix this to connect to server
 pollenDir<- "~/Dropbox/Research/Community Paleomodels/projects/pollen/output/all data by site/"
 
-files<- list.files(path=pollenDir, pattern="rel.genus.sum")
-sites<- gsub(".rel.genus.sum.csv", "", files)
+files<- list.files(path=pollenDir, pattern="gdm.data")
+sites<- gsub(".gdm.data.csv", "", files)
+
+times<- seq(0, 21000, by=500)
+
+dat<- read.csv(paste(pollenDir, files[i], sep=""))
